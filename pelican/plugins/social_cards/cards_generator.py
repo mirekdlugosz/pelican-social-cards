54987275861e485267f7c9982bead756964fb3fe
Mirek Długosz <mirek@mirekdlugosz.com>
2021-03-04 10:58:17 +0100

    Refactor generator into separate class

---
+++ pelican/plugins/social_cards/cards_generator.py
@@ -0,0 +1,137 @@
+import html
+import logging
+import textwrap
+
+from PIL import Image, ImageDraw, ImageFont
+from smartypants import smartypants
+
+from .settings import PLUGIN_SETTINGS
+
+logger = logging.getLogger(__name__)
+
+# FIXME: constants should be settings
+
+LEADING = 15
+
+# FIXME: better API:
+# - CANVAS_WIDTH - preferred canvas width
+# - CANVAS_HEIGHT - prefeered canvas height
+# - CANVAS_LEFT - distance from left border to canvas left border
+# - CANVAS_TOP - distance from top border to canvas top border
+
+CANVAS_HORIZONTAL_MARGIN = 40
+CANVAS_WIDTH = 1200 - CANVAS_HORIZONTAL_MARGIN * 2
+CANVAS_HEIGHT = 382
+CANVAS_TOP_MARGIN = 630 - CANVAS_HEIGHT
+
+
+def get_article_title(article):
+    metadata_title = getattr(article, f"{PLUGIN_SETTINGS['KEY_NAME']}_text", None)
+    if metadata_title:
+        return metadata_title.split("\\n")
+
+    # FIXME: rewrite this part, don't re-read file
+    if not article.settings.get("TYPOGRIFY"):
+        title = article.metadata.get("title")
+    else:
+        with open(article.source_path, encoding="UTF-8") as fh:
+            for line in fh:
+                if line.lower().startswith("title:"):
+                    _, title = line.split(":", 1)
+                    break
+    title = html.unescape(smartypants(title.strip()))
+    # FIXME: width should be in settings
+    title = textwrap.wrap(title, width=30)
+    return title
+
+
+class TextBox:
+    def __init__(self, text, font):
+        self._text = text
+        self._font = font
+        self.width = 0
+        self.height = 0
+        self.lines = 0
+        self.line_height = 0
+        self.line_dimensions = {}
+
+        self._compute_values()
+
+    def _compute_values(self):
+        max_width = 0
+        max_height = 0
+
+        for line in self._text:
+            font_width, font_height = self._font.getsize(line)
+            self.line_dimensions[line] = {
+                "width": font_width,
+                "height": font_height,
+            }
+            max_width = max(font_width, max_width)
+            max_height = max(font_height, max_height)
+        self.width = max_width
+        self.line_height = max_height + LEADING
+        self.lines = len(self._text)
+        self.height = self.line_height * self.lines - LEADING
+
+
+class CardsGenerator:
+    _font = None
+    _template = None
+
+    def __init__(self):
+        if not self._font:
+            type(self)._font = ImageFont.truetype(
+                PLUGIN_SETTINGS["FONT_FILENAME"], size=PLUGIN_SETTINGS["FONT_SIZE"]
+            )
+
+        if not self._template:
+            type(self)._template = Image.open(PLUGIN_SETTINGS["TEMPLATE"])
+
+    def _get_card_path(self, content_object):
+        card_stem = (
+            content_object.save_as.replace("/index.html", "")
+            .replace("/", "-")
+            .strip("-")
+        )
+        card_name = f"{card_stem}.png"
+        card_path = PLUGIN_SETTINGS["PATH"] / card_name
+        return card_path
+
+    def _generate_card_image(self, text):
+        img = self._template.copy()
+        draw = ImageDraw.Draw(img)
+        text_box = TextBox(text, self._font)
+        font_fill = PLUGIN_SETTINGS["FONT_FILL"]
+
+        # FIXME: different vertical and horizontal alignments
+        # FIXME: warning, if text_box sizes are bigger than canvas
+        current_y = ((CANVAS_HEIGHT - text_box.height) // 2) + CANVAS_TOP_MARGIN
+        for line in text:
+            current_x = (
+                (CANVAS_WIDTH - text_box.line_dimensions[line]["width"]) // 2
+            ) + CANVAS_HORIZONTAL_MARGIN
+            if current_x < 0:
+                logger.error(
+                    f"calculated negative x margin for '{line}', resetting to 0"
+                )
+                current_x = 0
+            draw.text((current_x, current_y), line, font=self._font, fill=font_fill)
+            current_y += text_box.line_height
+        return img
+
+    def create_for_object(self, content_object):
+        target_path = self._get_card_path(content_object)
+
+        attr_name = f"{PLUGIN_SETTINGS['KEY_NAME']}_source"
+        setattr(content_object, attr_name, target_path.as_posix())
+
+        # FIXME: we might need a way to force overwriting anyway
+        if target_path.exists():
+            logger.debug(f"Refusing to overwrite existing {target_path}")
+            return
+
+        article_title = get_article_title(content_object)
+
+        img = self._generate_card_image(article_title)
+        img.save(target_path)
--- pelican/plugins/social_cards/social_cards.py
+++ pelican/plugins/social_cards/social_cards.py
@@ -5,65 +5,17 @@ pelican.plugins.social_cards
 Plugin to generate social media cards with post title embedded
 """
 
-import html
 import logging
 from pathlib import Path
-import textwrap
 
-from PIL import Image, ImageDraw, ImageFont
 from pelican.generators import ArticlesGenerator, StaticGenerator
-from smartypants import smartypants
 
 from pelican import signals
 
+from .cards_generator import CardsGenerator
 from .settings import PLUGIN_SETTINGS, populate_plugin_settings
 
 logger = logging.getLogger(__name__)
-
-# FIXME: constants should be settings
-
-LEADING = 15
-
-# FIXME: better API:
-# - CANVAS_WIDTH - preferred canvas width
-# - CANVAS_HEIGHT - prefeered canvas height
-# - CANVAS_LEFT - distance from left border to canvas left border
-# - CANVAS_TOP - distance from top border to canvas top border
-
-CANVAS_HORIZONTAL_MARGIN = 40
-CANVAS_WIDTH = 1200 - CANVAS_HORIZONTAL_MARGIN * 2
-CANVAS_HEIGHT = 382
-CANVAS_TOP_MARGIN = 630 - CANVAS_HEIGHT
-
-
-class TextBox:
-    def __init__(self, text, font):
-        self._text = text
-        self._font = font
-        self.width = 0
-        self.height = 0
-        self.lines = 0
-        self.line_height = 0
-        self.line_dimensions = {}
-
-        self._compute_values()
-
-    def _compute_values(self):
-        max_width = 0
-        max_height = 0
-
-        for line in self._text:
-            font_width, font_height = self._font.getsize(line)
-            self.line_dimensions[line] = {
-                "width": font_width,
-                "height": font_height,
-            }
-            max_width = max(font_width, max_width)
-            max_height = max(font_height, max_height)
-        self.width = max_width
-        self.line_height = max_height + LEADING
-        self.lines = len(self._text)
-        self.height = self.line_height * self.lines - LEADING
 
 
 def is_plugin_configured():
@@ -74,27 +26,6 @@ def should_skip_object(content_object):
     return hasattr(content_object, PLUGIN_SETTINGS["KEY_NAME"])
 
 
-def get_article_title(article):
-    metadata_title = getattr(article, f"{PLUGIN_SETTINGS['KEY_NAME']}_text", None)
-    if metadata_title:
-        return metadata_title.split("\\n")
-
-    # FIXME: rewrite this part, don't re-read file
-    if not article.settings.get("TYPOGRIFY"):
-        title = article.metadata.get("title")
-    else:
-        with open(article.source_path, encoding="UTF-8") as fh:
-            for line in fh:
-                if line.lower().startswith("title:"):
-                    _, title = line.split(":", 1)
-                    break
-    title = html.unescape(smartypants(title.strip()))
-    title = textwrap.wrap(
-        title, width=30
-    )  # FIXME: we need smarter way, that would use rendered font width
-    return title
-
-
 def create_paths_map(staticfiles):
     return {
         static_file.source_path: static_file.save_as
@@ -103,74 +34,19 @@ def create_paths_map(staticfiles):
     }
 
 
-def generate_thumbnail_image(template, text, output_path, context):
-    font = context["image_font"]
-    font_fill = context["FONT_FILL"]
-
-    draw = ImageDraw.Draw(template)
-    text_box = TextBox(text, font)
-
-    # FIXME: different vertical and horizontal alignments
-    # FIXME: warning, if text_box sizes are bigger than canvas
-
-    current_y = ((CANVAS_HEIGHT - text_box.height) // 2) + CANVAS_TOP_MARGIN
-    for line in text:
-        current_x = (
-            (CANVAS_WIDTH - text_box.line_dimensions[line]["width"]) // 2
-        ) + CANVAS_HORIZONTAL_MARGIN
-        if current_x < 0:
-            logger.error(f"calculated negative x margin for '{line}', resetting to 0")
-            current_x = 0
-        draw.text((current_x, current_y), line, font=font, fill=font_fill)
-        current_y += text_box.line_height
-    template.save(output_path)
-
-
-def generate_thumbnail_for_object(content_object, context):
-    article_title = get_article_title(content_object)
-
-    thumbnail_stem = (
-        content_object.save_as.replace("/index.html", "").replace("/", "-").strip("-")
-    )
-    thumbnail_name = f"{thumbnail_stem}.png"
-    thumbnail_path = context["PATH"] / thumbnail_name
-
-    setattr(
-        content_object,
-        f"{PLUGIN_SETTINGS['KEY_NAME']}_source",
-        thumbnail_path.as_posix(),
-    )
-
-    if (
-        thumbnail_path.exists()
-    ):  # FIXME: we might need a way to force overwriting anyway
-        logger.debug(f"Refusing to overwrite existing {thumbnail_path}")
-        return
-
-    template = context["image_template"].copy()
-
-    generate_thumbnail_image(template, article_title, thumbnail_path, context)
-
-
-def generate_thumbnails(article_generator):
+def generate_cards(generator):
     if not is_plugin_configured():
         return
 
     PLUGIN_SETTINGS["PATH"].mkdir(exist_ok=True)
 
-    template = Image.open(PLUGIN_SETTINGS["TEMPLATE"])
-    image_font = ImageFont.truetype(
-        PLUGIN_SETTINGS["FONT_FILENAME"], size=PLUGIN_SETTINGS["FONT_SIZE"]
-    )
+    cards_generator = CardsGenerator()
 
-    additional_context = {"image_template": template, "image_font": image_font}
-
-    context = {**PLUGIN_SETTINGS, **additional_context}
-
-    for article in article_generator.articles:
+    # FIXME: drafts, translations, pages...
+    for article in generator.articles:
         if should_skip_object(article):
             continue
-        generate_thumbnail_for_object(article, context)
+        cards_generator.create_for_object(article)
 
 
 def attach_metadata(finished_generators):
@@ -202,5 +78,5 @@ def attach_metadata(finished_generators):
 
 def register():
     signals.initialized.connect(populate_plugin_settings)
-    signals.article_generator_finalized.connect(generate_thumbnails)
+    signals.article_generator_finalized.connect(generate_cards)
     signals.all_generators_finalized.connect(attach_metadata)
